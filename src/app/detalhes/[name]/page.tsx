976fd666541bf538eca17c338383aa6474821518
rhanierymueller <rhanimueller@hotmail.com>
2026-03-29 15:11:28 -0300

    changes

--- src/app/detalhes/[name]/page.tsx
+++ src/app/detalhes/[name]/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 
+import { Box } from '@mui/material';
 import { use, useEffect, useState } from 'react';
+import './page.css';
 
 interface DetalhesProps {
   params: Promise<{
@@ -8,25 +10,38 @@ interface DetalhesProps {
   }>;
 }
 
+interface AbilityItem {
+  ability: {
+    name: string;
+  };
+}
+
+interface Details {
+  abilities: AbilityItem[];
+  name: string;
+}
+
 function Detalhes({ params }: DetalhesProps) {
   const { name } = use(params);
-  const [details, setDetails] = useState(null)
+  const [details, setDetails] = useState<Details | null>(null);
 
   useEffect(() => {
     const getMoreInformation = async () => {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-      const data = await response.json()
-      setDetails(data)
-      console.log(data)
-  }
-  
-  getMoreInformation()
-  }, [name])
-  
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+      const data = await response.json();
+      setDetails(data);
+      console.log(data);
+    };
+
+    getMoreInformation();
+  }, [name]);
+
   return (
-    <div>
-      <p>{name}</p>
-    </div>
+    <Box className="container">
+      {details?.abilities.map((detail: AbilityItem) => (
+        <p key={detail.ability.name}>{detail.ability.name}</p>
+      ))}
+    </Box>
   );
 }
 
